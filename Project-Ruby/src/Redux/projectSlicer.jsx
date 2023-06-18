acc5edb9d65cdca606b7c987480740a650168d53
MichaelIgb <igbanugo.michael@yahoo.com>
2023-06-18 14:40:27 -0400

    added redux store foundations

---
+++ Project-Ruby/src/Redux/projectSlicer.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    projects: [{
+        projectName: '',
+        projectDescription: '',
+        serviceType: '',
+        projectImages: ['','']
+    }]
+}
+
+export const projectSlicer = createSlice({
+    name: 'project',
+    initialState,
+
+    reducers: {
+        addUser: (state, action) => { state.knownUsers.push(action.payload) },
+        login: (state, action) => { state.currentUserInfo = { ...action.payload, isLoggedIn: true } },
+        logout: (state) => {state.currentUserInfo.isLoggedIn === true ? state.currentUserInfo.isLoggedIn = false : state.currentUserInfo.isLoggedIn = true},
+    }
+})
+
+export const { addUser, login, logout } = projectSlicer.actions
+
+export default projectSlicer.reducer
---
+++ Project-Ruby/src/Redux/userSlicer.jsx
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    currentUserInfo: {
+        isLoggedIn: false,
+        userName: '',
+        userEmail: '',
+    },
+    knownUsers: [
+        {
+          userName: 'Michaelci',
+          userEmail: 'igbanugo.michael@yahoo.com',
+          userPassword: 'Starter77',  
+        },
+    ]
+}
+
+export const userSlicer = createSlice({
+    name: 'user',
+    initialState,
+
+    reducers: {
+        addUser: (state, action) => { state.knownUsers.push(action.payload) },
+        login: (state, action) => { state.currentUserInfo = { ...action.payload, isLoggedIn: true } },
+        logout: (state) => {state.currentUserInfo.isLoggedIn === true ? state.currentUserInfo.isLoggedIn = false : state.currentUserInfo.isLoggedIn = true},
+    }
+})
+
+export const { addUser, login, logout } = userSlicer.actions
+
+export default userSlicer.reducer
